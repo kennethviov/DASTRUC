73397d0f363cbca26979094e3b4f0ab0327fc17d
Kenneth Reniel Viovicente <151752526+kennethviov@users.noreply.github.com>
2024-10-07 20:33:21 +0800

    Update Node.java

--- InfixToPostfix/Node.java
+++ InfixToPostfix/Node.java
@@ -11,72 +11,54 @@ class Node<E> {
 class Stack<E> {
     Node<E> head;
     private int count;
-
+    
     public Stack() {
         head = null;
-        count= 0;
+        count = 0;
+    }
+
+    public Stack(Node<E> head) {
+        this.head = head;
+        count = 0;
     }
 
     void push(E data) {
-        Node<E> n = new Node<E>(data);
-
-        if (head == null) {
-            head = n;
-        } else {
-            Node<E> curr = head;
-            while (curr.next != null) {
-                curr = curr.next;
-            }
-            curr.next = n;
-        }
+        Node<E> node = new Node<>(data);
+        node.next = head;
+        head = node;
         count++;
-        //System.out.println("Added " + data);
     }
 
     E pop() {
-        if (head == null) {
-            System.out.println("No data in the list.");
-            return null;
-        } else if (head.next == null) {
+        if (!isEmpty()) {
             E data = head.data;
-            head = null;
-            count--;
-            return data;
-        } else {
-            Node<E> curr = head;
-            while (curr.next.next != null) {
-                curr = curr.next;
-            }
-            E data = curr.next.data;
-            curr.next = null;
+            head = head.next;
             count--;
             return data;
         }
+        return null;
     }
 
     E peek() {
-        if (head == null) {
-            System.out.println("No data in the list.");
-            return null;
-        } else {
-            Node<E> curr = head;
-            while (curr.next != null) {
-                curr = curr.next;
-            }
-            return curr.data;
-        }
-    }
-
-    int getCount() {
-        return count;
+        if (!isEmpty())
+            return head.data;
+        return null;
     }
 
     void display() {
         Node<E> curr = head;
-
+        
         while(curr != null) {
             System.out.println("[" + curr.data + "]");
             curr = curr.next;
         }
+    }
+    
+    int getCount() {
+        return count;
+    }
+
+    boolean isEmpty() {
+        return head == null || count == 0;
     }
 }
